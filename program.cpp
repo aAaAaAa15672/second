cfe5eeddf8be2b4ffc4ebd2f3a14bbdc066915e3
aAaAaAa15672 <71511438+aAaAaAa15672@users.noreply.github.com>
2023-04-27 19:01:48 +0500

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,11 +1,19 @@
 #include "graphics.h"
 #include "picture.hpp"
-int main()
-{ initwindow(800,600);
- house(); // дом
- sun(); // солнце
- man(); // человек
- fence(); // забор
- getch();
- closegraph();
+
+void treug(int x, int y, int a, int color){
+   int v[] = {x, y, x+a/2, y-a/2,x+a, y};
+   setcolor(color);
+   setfillstyle(SOLID_FILL, color);
+   fillpoly(3, v);
+}
+
+int main(){ 
+   initwindow(800,600);
+   house(); // дом
+   sun(); // солнце
+   man(); // человек
+   fence(); // забор
+   getch();
+   closegraph();
 }
