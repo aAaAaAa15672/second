760f3899b0018fe4fff811a33cc230007ee96062
iloveforest <131958323+iloveforest@users.noreply.github.com>
2023-04-27 22:38:23 +0500

    Update fence.cpp

--- fence.cpp
+++ fence.cpp
@@ -1,1 +1,17 @@
+#include "graphics.h"
+#include "picture.hpp"
 
+void doska(int a, int b, int c, int d){
+  setcolor(BROWN);
+  setfillstyle(SOLID_FILL, COLOR(218, 165, 32));
+  bar(a, b, c, d);
+}
+
+void fence(){
+  int i,j = 15;
+  for(i = 0; i <= 1280; i += 25){
+    doska(i, 400, j, 500);
+    j += 25;
+  }
+  bar(0, 440, 1280, 445); 
+}
