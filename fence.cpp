5b467d84088180da5f88043357696e61d2c7568a
aAaAaAa15672 <71511438+aAaAaAa15672@users.noreply.github.com>
2023-04-30 17:12:04 +0500

    Update fence.cpp

--- fence.cpp
+++ fence.cpp
@@ -6,8 +6,8 @@ void fence(){
   setfillstyle(SOLID_FILL, COLOR(218, 165, 32));
   int i,j = 15;
   for(i = 0; i <= 1280; i += 25){
-    bar(i, 400, j, 500);
+    bar(i, 500, j, 600);
     j += 25;
   }
-  bar(0, 440, 1280, 445); 
+  bar(0, 540, 1280, 545); 
 }
