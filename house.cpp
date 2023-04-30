f4b5020a06611b48b42e78b22155e0bd9b8e0daf
aAaAaAa15672 <71511438+aAaAaAa15672@users.noreply.github.com>
2023-04-30 16:58:23 +0500

    Update house.cpp

--- house.cpp
+++ house.cpp
@@ -1,10 +1,16 @@
 #include "graphics.h"
 #include "picture.hpp"
 
-void house (int x, int y, int h){
+void house (){
+   int x = 800*1/9;
+   int y = 600*2/3; 
+   int h = 600/5;
    setfillstyle(SOLID_FILL, COLOR(128,64,48));
    bar(x,y,x+100,y+100);
-   treug(x-50,y,200, RED);
+   int v[] = {x-50, y, x+200/2, y-200/2,x+200, y};
+   setcolor(RED);
+   setfillstyle(SOLID_FILL, RED);
+   fillpoly(3, v);
    setfillstyle(BKSLASH_FILL, WHITE);
    bar(x+33, y+33, x+66, y+66);
 }
