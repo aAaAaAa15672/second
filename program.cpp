20208a82c6433c204f3c2ec948cf4276208d7021
aAaAaAa15672 <71511438+aAaAaAa15672@users.noreply.github.com>
2023-04-30 16:58:39 +0500

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,13 +1,6 @@
 #include "graphics.h"
 #include "picture.hpp"
 
-void treug(int x, int y, int a, int color){
-   int v[] = {x, y, x+a/2, y-a/2,x+a, y};
-   setcolor(color);
-   setfillstyle(SOLID_FILL, color);
-   fillpoly(3, v);
-}
-
 int main(){ 
    initwindow(800,600);
    house(); // дом
